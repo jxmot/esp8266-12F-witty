c0824c9e1a798dfb42e6a44f49c4f89d2b5edeae
jxmot <webdev@stupiddog.net>
2019-02-08 22:13:34 -0600

    initial commit of project

--- README.md
+++ README.md
@@ -1,1 +1,111 @@
-esp8266-12F-witty
+# Witty Board Sketch
+This sketch contains code that demonstrates the use of a Witty(GizWits) ESP8266-12F & breakout board.
+
+The "Witty" board is similar to many other boards with the exception of containing an LDR, 3-wire RGB LED, and a momentary push button. And like most of the other boards it comes equipped with a daughter board that provides a USB<->serial(CH340G) connection, and a flash and reset button for flashing.
+
+# History
+
+I happened to find a couple of the Witty boards for $5 each. And the ESP8266-12F is a variant that I haven't used yet. So why not?
+
+# Audience Requirements
+
+This project will require *some* knowledge of the ESP8266 and - 
+
+* how to use the Arduino IDE or other tool for preparing a sketch for execution
+* how to compile and download a sketch
+* a basic understanding of the `setup()` and `loop()` functions of the ESP8266
+* a basic understanding of 3-wire RGB LEDs
+* a basic understanding of what an LDR(*light dependent resistor, aka "photocell"*) is
+* a basic understanding of what PWM (*pulse width modulation*) is
+* how to install the driver(*only if you  need it*) for the **`CH340G`**, it is the circuit that allows communication with the board via USB.
+
+# Sketch Overview
+
+The sketch exercises the RBG LED, LDR and the push button. It has the option of using PWM or digital I/O to operate the LEDs. The code is commented to make it easy to adjust the options to get different effects from the LEDs.
+
+## Miscellaneous Details
+
+There are some `#define` statements that can be used for adjusting specific aspects of the sketch. They are - 
+
+* use PWM or digital I/O to run the LEDs :
+    *  `USE_PWM`
+    *  `USE_DIG`
+
+Find the call to `resetLED()`,  it's the last function call in `setup()`. Change the argument to one of the following - 
+
+`    resetLED(USE_PWM)` - Use PWM and calls to `analogWrite()` to operate the LEDs.
+`    resetLED(USE_DIG)` - Uses digial I/O calls to `digitalWrite()` to operate the LEDs.
+
+* enable some additional PWM functionality :
+    * `PWM_TABLE` - when `#define`d as `true` the sketch will adjust the LED colors using a table of RGB values.
+
+<br>
+
+* to enable or disable additional output to the console :
+    * `DBGOUT` - when `true` additional information will be sent to the console, if `false` the extra output is muted.
+    
+There are some `#define`d values that can be used to change the timing of the LEDs :
+
+* PWM operation -
+    * `LED_INTERVAL` - the amount of time in milliseconds that is between LED operations
+    * `PWMSTEP` - the amount that the PWM step value changes
+    * `LDR_INTERVAL` - the amount of time in milliseconds that is between LED
+    * `PWM_ZERO_DELAY` - the amount of time that the LEDs will stay off during a complete cycle of dark to light
+    * `PWM_MAX_DELAY` - the amount of time that the LEDs will stay on before returning to an off state
+
+* digital I/O operations - 
+    * `DIGLED_FASTEST` 
+    * `DIGLED_FAST`
+    * `DIGLED_SLOW`
+    * `DIGLED_SLOWER`
+    * `DIGLED_SLOWEST`
+    * `DIGLED_DELAY` - this can be `#define`d as one of the values above, they range from 1 to 1000 milliseconds. It is the amount of time that the LEDs will stay in a particular pattern.
+
+## LED Operations
+
+The PWM operation of the LEDs is a graduated change in state from off to full on. This is accomplished by increasing the PWM value over time. 
+
+The digital I/O operation is different. It's because there is no control over the brightness of the LEDs. They're either on or off. Instead a 3 bit pattern is *rotated* through the LEDs. For example:
+
+|  Step |  Red |  Grn |  Blu |
+|:-----:|:----:|:----:|:----:|
+| Start | HIGH |  LOW |  LOW |
+|       |   1  |   0  |   0  |
+|   1   |   0  |   1  |   0  |
+|   2   |   0  |   0  |   1  |
+|   3   |   1  |   0  |   0  |
+
+HIGH or 1 = the LED is on
+
+Starting with different patterns have somewhat different effects.
+
+|  Red |  Grn |  Blu |
+|:----:|:----:|:----:|
+| HIGH |  LOW | HIGH |
+|   1  |   0  |   1  | red + blue
+|   1  |   1  |   0  | red + green
+|   0  |   1  |   1  | green + blue
+
+
+|  Red |  Grn |  Blu |
+|:----:|:----:|:----:|
+| HIGH | HIGH |  LOW |
+|   1  |   1  |   0  | red + green
+|   0  |   1  |   1  | green + blue
+|   1  |   0  |   1  | red + blue
+
+
+*Note: The green LED on the board I used was much brighter than the red and the blue. A mix of all three LEDs should produce white, but mine did no.*
+
+## Button & LDR
+
+## The loop
+
+The majority of sketch's logic is executed from within the `loop()` function. In this skectch that isn't an issue only because this sketch is simple and doesn't require any precision timing or fast execution of logic within `loop()`.
+
+## Other Things to Note
+
+
+
+
+
---
+++ esp8266-12F-witty.ino
@@ -0,0 +1,270 @@
+/*
+    esp8266-12F-witty.ino - demo sketch intended to
+    run on the "Witty" board (ESP8266-12F, with a 
+    3-wire RGB LED, LDR(photocell), and a push button.
+*/
+// methods for activating the LEDs
+#define USE_PWM true
+#define USE_DIG false
+// sub-demos of the LEDs 
+#define PWM_TABLE true
+// pins
+#define BUTTON  4
+#define RED_LED 15
+#define GRN_LED 12
+#define BLU_LED 13
+#define LDR_IN  A0
+// change to 'true' to see more console output
+#define DBGOUT false
+// LED timing
+#if DBGOUT
+//      500ms between LED state changes, note
+//      that the LED static time value will have
+//      an effect on the appearance. the static
+//      times are: PWM_ZERO_DELAY & PWM_MAX_DELAY,
+//      and DIGLED_DELAY
+#define LED_INTERVAL    500
+//      change the PWM ratio by 10
+#define PWMSTEP         10
+//      250ms between LDR(analog) readings
+#define LDR_INTERVAL    250
+// run-time delays
+#define PWM_ZERO_DELAY  3000
+#define PWM_MAX_DELAY   2000
+#else   // DBGOUT
+#define LED_INTERVAL    25
+#define PWMSTEP         1
+#define LDR_INTERVAL    25
+#define PWM_ZERO_DELAY  1500
+#define PWM_MAX_DELAY   1000
+#endif  // DBGOUT
+
+// the amount of time (in milliseconds) that
+// the LED will stay in its current state. note
+// that the LED_INTERVAL value will have an 
+// effect on the appearance.
+#define DIGLED_FASTEST  1
+#define DIGLED_FAST     50
+#define DIGLED_SLOW     250
+#define DIGLED_SLOWER   500
+#define DIGLED_SLOWEST  1000
+#define DIGLED_DELAY    DIGLED_SLOWER
+
+// indices into the PWM color table
+#define PWMLED_RED      0
+#define PWMLED_GRN      1
+#define PWMLED_BLU      2
+
+// for choosing the method for activating the LEDs
+//      default is PWM, USE_DIG is the other choice
+bool ledMethod = USE_PWM;
+// set time for the next LED access
+//      LED_INTERVAL milliseconds in the future
+int nextLED = LED_INTERVAL + millis();
+//      0 = LED is off
+int pwm = 0;
+//      0 = R,G, or B is OFF
+//      for digital control only, the code will determine
+//      how the individual LEDs are turned on or off
+// 
+//      different patterns have somewhat different effects,
+//      the green led on the board I used was much brighter
+//      than the other two. so most of the time it looked
+//      very green.
+//int rgbLED[3] = {HIGH, LOW, LOW};
+//int rgbLED[3] = {HIGH, LOW, HIGH};
+int rgbLED[3] = {HIGH, HIGH, LOW};
+
+int pwmLED[8][3] = {
+    {255,  0,  0},  // red 
+    {  0,255,  0},  // green 
+    {  0,  0,255},  // blue 
+    {255,255,255},  // white
+    {237,109,  0},  // orange
+    {  0, 46, 90},  // indigo
+    {255,  0,255},  // magenta
+    {  0,  0,  0}   // off
+};
+// set time for the next LDR reading
+//      LDR_INTERVAL milliseconds in the future
+int nextLDR = LDR_INTERVAL + millis();
+int LDRvalue = 0;
+// loop() will toggle between the LED and LDR
+bool toggle = false;
+// The "witty" board will start with the button HIGH
+int lastButton = LOW;
+/**********************************************************
+*/
+void setup() 
+{
+    // initialize and output some blank lines
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println();
+    Serial.println();
+
+    // initialize the button...
+    initButton();
+
+    // reset all 3 colors to OFF, and use digital
+    // 
+    resetLED(USE_PWM);
+//    resetLED(USE_DIG);
+}
+
+void loop()
+{
+    yield();
+
+    readButton();
+
+    if(toggle) runLED(ledMethod);
+    else readLDR();
+
+    toggle = !toggle;
+}
+
+/*****************************************
+    Initialize the button input to use a
+    pull-up. That means that when the 
+    button isn't pressed the input will
+    read as HIGH. When the button is pressed
+    it will ground the line and a LOW will
+    be read.
+*/
+void initButton()
+{
+    pinMode(BUTTON, INPUT_PULLUP);
+}
+
+/*
+    Read the button state. If it is different
+    from the last time it was read then its
+    state has changed. Don't continue to 
+    announcing the button state if it has not
+    changed.
+*/
+void readButton()
+{
+int buttnow;
+
+    // read the current state of the button
+    buttnow = digitalRead(BUTTON);
+    // if the button is pressed and the last 
+    // time the button was read it was HIGH then...
+    if((buttnow == LOW) && (lastButton != LOW))
+    {
+        if(DBGOUT) 
+        {
+            // announce the change in the button state
+            Serial.println("BUTT: LOW " + String(buttnow));
+        } else {
+            Serial.println("LDR : "+String(LDRvalue));
+            if(ledMethod == USE_PWM) Serial.println("LEDS: "+String(pwm));
+            else Serial.println("LEDS: "+String(rgbLED[0])+" "+String(rgbLED[1])+" "+String(rgbLED[2]));
+        }
+    }
+    // if the button is released and the last 
+    // time the button was read it was LOW then...
+    if((buttnow == HIGH) && (lastButton != HIGH))
+    {
+        // announce the change in the button state & PWM
+        Serial.println("BUTT: HIGH " + String(buttnow));
+    }
+    // save the button state
+    lastButton = buttnow;
+}
+
+/*
+    Manage the LED states
+*/
+void runLED(bool pwmORdig)
+{
+int temp;
+
+    // is it time to change the LED?
+    if(nextLED < millis())
+    {
+        if(pwmORdig == USE_PWM)
+        {
+////
+            if(DBGOUT) Serial.println("LEDS: "+String(pwm));
+
+            // yep, adjust the PWM of each of the colors
+            analogWrite(RED_LED, pwm);
+            analogWrite(GRN_LED, pwm);
+            analogWrite(BLU_LED, pwm);
+            // when the PWM is OFF wait for a while
+            if(pwm == 0) delay(PWM_ZERO_DELAY);
+            // increment the PWM value
+            pwm = pwm + PWMSTEP;
+            // did we reach the end of the PWM range?
+            if(pwm > PWMRANGE) 
+            {
+                // yep, set the PWM value to OFF(0) and 
+                // leave the LED turned ON for a while
+                pwm = 0;
+                delay(PWM_MAX_DELAY);
+            }
+////
+        } else {
+////
+            // write the pattern of ON/OFF to the LEDs
+            digitalWrite(RED_LED, rgbLED[0]);
+            digitalWrite(GRN_LED, rgbLED[1]);
+            digitalWrite(BLU_LED, rgbLED[2]);
+            // rotate the pattern through the 3 LEDs
+            temp = rgbLED[2];
+            rgbLED[2] = rgbLED[1];
+            rgbLED[1] = rgbLED[0];
+            rgbLED[0] = temp;
+
+            delay(DIGLED_DELAY);
+////
+        }
+        // get the time of the next LED change...
+        nextLED = LED_INTERVAL + millis();
+    }
+}
+
+/*
+    Reset the LEDs to OFF
+*/
+void resetLED(bool pwmORdig)
+{
+    ledMethod = pwmORdig;
+
+    if(pwmORdig == USE_PWM)
+    {
+        analogWrite(RED_LED, 0);
+        analogWrite(GRN_LED, 0);
+        analogWrite(BLU_LED, 0);
+    } else {
+        pinMode(RED_LED, OUTPUT);
+        digitalWrite(RED_LED, LOW);
+        delay(100);
+        pinMode(GRN_LED, OUTPUT);
+        digitalWrite(GRN_LED, LOW);
+        delay(100);
+        pinMode(BLU_LED, OUTPUT);
+        digitalWrite(BLU_LED, LOW);
+        delay(100);
+    }
+}
+
+/*
+    Read the LDR if it's time
+*/
+void readLDR()
+{
+    // is it time to read the LDR?
+    if(nextLDR < millis())
+    {
+        // yep, read it
+        LDRvalue = analogRead(LDR_IN);
+        if(DBGOUT) Serial.println("LDR : "+String(LDRvalue));
+        // get the time of the next LDR reading...
+        nextLDR = LDR_INTERVAL + millis();
+    }
+}
+
